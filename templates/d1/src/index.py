732a1712c2630f332d1fa2979328536bd97a9b68
Gabriel Massadas <gmassadas@cloudflare.com>
2025-12-15 14:48:58 +0000

    Optimize imports in templates for better workers snapshots

--- templates/d1/src/index.py
+++ templates/d1/src/index.py
@@ -1,7 +1,7 @@
 from workers import WorkerEntrypoint
 from django_cf import DjangoCF
+from app.wsgi import application
 
 class Default(DjangoCF, WorkerEntrypoint):
     def get_app(self):
-        from app.wsgi import application
         return application
